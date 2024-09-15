15b93a95e2c82f5b592c239dcf79f05d3eb284a4
NACIMSAAFI <7715@holbertonstudents.com>
2024-09-15 13:36:16 +0100

    Task 14

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,31 @@
+function createElement(data) {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = data;
+    document.body.appendChild(paragraph);
+  }
+  
+  function queryWikipedia(callback) {
+    const xhr = new XMLHttpRequest();
+    const url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*';
+  
+    xhr.open('GET', url, true);
+    xhr.onload = function() {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        const response = JSON.parse(xhr.responseText);
+        const page = response.query.pages;
+        const pageId = Object.keys(page)[0];
+        const extract = page[pageId].extract;
+  
+        callback(extract);
+      } else {
+        console.error('Failed to fetch data from Wikipedia');
+      }
+    };
+    xhr.onerror = function() {
+      console.error('Request error');
+    };
+    xhr.send();
+  }
+  
+  queryWikipedia(createElement);
+  
